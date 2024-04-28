f9aaf63c891e3c296c14fd9f9942712aeedbd8f3
sandra <nasdrahlovejinx@hotmail.com>
2024-04-28 21:05:29 +0200

    preparing for deploy in vercel

--- server/index.js
+++ server/index.js
@@ -1,41 +1,36 @@
-const express=require('express');
-const dotenv=require('dotenv').config()
-const cors=require('cors')
-const {mongoose}=require('mongoose')
-const cookieParser=require('cookie-parser')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const shoeRoutes = require('./routes/shoeRoutes');
 const collectionRoutes = require('./routes/collectionRoutes');
 const offersRoutes = require('./routes/offersRoutes');
-const authRoutes=require('./routes/authRoutes')
+const authRoutes = require('./routes/authRoutes');
 const shoeController = require('./controllers/shoeController');
-const app=express();
 
-//connection Database
+const app = express();
+
+// Connection to Database
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
         console.log("Database connected!");
-        // Insertar zapatillas, colecciones y ofertas iniciales
         shoeController.insertInitialData();
     })
     .catch((err) => {
-        console.log("Database not connected", err);
+        console.error("Database connection failed", err);
     });
 
-//middlware
-app.use(express.json())
+// Middleware
+app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 
-//routes
-app.use('/',authRoutes)
+// Routes
+app.use('/', authRoutes);
 app.use('/', shoeRoutes);
 app.use('/', collectionRoutes);
 app.use('/', offersRoutes);
 
-//port 
-const port=8000;
-app.listen(port,()=> console.log(`Server is running on port ${port}`))
-
-
-
-
+// Export the app
+module.exports = app;
